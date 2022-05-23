c33439be8f612764171e9852f093c3bcceb018b8
MegaWAT <gokuvz@wp.pl>
2022-05-23 16:25:28 +0200

    Add files via upload

--- pppvvvppp.js
+++ pppvvvppp.js
@@ -1217,3 +1217,7 @@ if(adimp){
 	
 	
 	}
+
+	console.clear();
+	console.log('%cSkrypt został poprawnie załadowany!','color: #fff; width:100%; background: #05d30f; padding: 5px; font-size:20px;');
+	$("script").last().remove();
